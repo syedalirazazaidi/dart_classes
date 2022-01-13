aab8a70f370a31a09a2f2ee94abe896cca721385
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2022-01-13 13:28:38 +0500

    Classes with private members

--- y_classes/bin/y_classes.dart
+++ y_classes/bin/y_classes.dart
@@ -1,36 +1,55 @@
-class Company {
-  //every class has a property
-  String empName = '';
-  int empAge = 0;
-  int totalEmployee = 0;
-  int empSalary = 0;
+// class Company {
+//   //every class has a property
+//   String _empName = '';
+//   int _empAge = 0;
+//   int _totalEmployee = 0;
+//   int _empSalary = 0;
 
-  Company({
-    required String empName,
-    required int empAge,
-    required int totalEmployee,
-    required int empSalary,
-  })  : this.empName = empName,
-        this.empAge = empAge,
-        this.totalEmployee = totalEmployee,
-        this.empSalary = empSalary;
+//   Company({
+//     required String empName,
+//     required int empAge,
+//     required int totalEmployee,
+//     required int empSalary,
+//   })  : this._empName = empName,
+//         this._empAge = empAge,
+//         this._totalEmployee = totalEmployee,
+//         this._empSalary = empSalary;
 
-  void printData() {
-    print('no of employee $empName');
-    print('no of employee $empAge');
-    print('no of employee $totalEmployee');
-    print('no of employee $empSalary');
-  }
-}
+//   void printData() {
+//     print('no of employee $_empName');
+//     print('no of employee $_empAge');
+//     print('no of employee $_totalEmployee');
+//     print('no of employee $_empSalary');
+//   }
+// }
+
+// void main() {
+//   Company companyName = Company(
+//     empName: 'zai',
+//     empAge: 32,
+//     totalEmployee: 23,
+//     empSalary: 23000,
+//   );
+//   companyName._empAge = 322;
+//   print(companyName._empName);
+//   print(companyName._empSalary);
+//   companyName.printData();
+// }
+import 'emplo.dart';
 
 void main() {
-  Company companyName = Company(
-    empName: 'zai',
+  Company mycompany = Company(
+    empName: 'ali',
     empAge: 32,
-    totalEmployee: 23,
-    empSalary: 23,
+    totalEmployee: 300,
+    empSalary: 60000,
   );
-  companyName.empAge = 322;
-  print(companyName.empName);
-  companyName.printData();
+  // mycompany.printData();
+  // print(mycompany.empFunc());
+  print(mycompany.empFunc);
+  mycompany.empFunc = 'zaidi';
+  print(mycompany.empFunc);
+  print(mycompany.ageFunc);
+  print(mycompany.totalFunc);
+  print(mycompany.salFunc);
 }
